462c218422d6e679076dcaa6810606d495de7182
thinkpad p52 fedora 42 <alvarojuanez.prgit01@gmail.com>
2025-11-22 01:37:12 -0400

    feat(web cocina):

---
+++ Lenguajes_de_marcas_LMSGI/UT02 Utilización de lenguajes de marcas en entornos web/web_cocina/js/app.js
@@ -0,0 +1,2 @@
+// Busca el elemento con id "year" y ponle el año actual
+document.getElementById("year").textContent = new Date().getFullYear();
---
+++ python/01_11_tresEnOrden/tresEnOrden.py
@@ -0,0 +1,11 @@
+import sys #Biblioteca standar
+
+def tresEnOrden():
+    #Obten los datos del usuario
+    num1 = int(input("Primero  \n"))
+    num2 = int(input("Segundo  \n"))
+    num3 = int(input("Tercero  \n"))
+
+ 
+
+tresEnOrden()   #Llama a la función
